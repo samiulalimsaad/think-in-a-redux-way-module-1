4929a42a6f76e8aac2310bd952ed687783b34329
samiulalimsaad <saadnbiuraj@gmail.com>
2022-08-14 11:21:53 +0600

    double

--- 1.1/script.js
+++ 1.1/script.js
@@ -2,10 +2,19 @@ const counterEl = document.querySelector("#counter");
 const incrementEl = document.querySelector("#increment");
 const decrementEl = document.querySelector("#decrement");
 
+const counter2El = document.querySelector("#counter2");
+const increment2El = document.querySelector("#increment2");
+const decrement2El = document.querySelector("#decrement2");
+
 let count = 0;
+let count2 = 0;
 
 const render = () => {
     counterEl.innerText = count;
+};
+
+const render2 = () => {
+    counter2El.innerText = count2;
 };
 
 incrementEl.onclick = () => {
@@ -16,4 +25,14 @@ incrementEl.onclick = () => {
 decrementEl.onclick = () => {
     --count;
     render();
+};
+
+increment2El.onclick = () => {
+    ++count2;
+    render2();
+};
+
+decrement2El.onclick = () => {
+    --count2;
+    render2();
 };
